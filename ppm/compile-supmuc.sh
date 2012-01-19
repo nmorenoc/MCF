89b26e797b42dfe9c420e4960f827927f2a21187
Xin Bian <lu64yuz3@i01r12s30.sm.lrz.de>
2012-01-19 17:43:34 +0100

    Compile ppm with ifort 11

--- ppm/compile-supmuc.sh
+++ ppm/compile-supmuc.sh
@@ -6,7 +6,10 @@
 module unload mpi.ibm
 module load mpi.intel
 
-make PREFIX=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/local_intelmpi_ifort121_o2_g \
+module unload fortran
+module load fortran/intel/11.1
+
+make PREFIX=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/local_intelmpi_ifort111_o2_g \
      BUILD_MAKE_FLAGS=-j16 FCFLAGS='-g -O2' \
-     WRKDIR=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/work_intelmpi_ifort121_o2_g \
+     WRKDIR=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/work_intelmpi_ifort111_o2_g \
      install_all_but_mpi
