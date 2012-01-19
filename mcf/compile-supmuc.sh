5b4f90e52d1ede44a45fedfb4ff5d921e7df9b1e
Xin Bian <lu64yuz3@i01r12s30.sm.lrz.de>
2012-01-19 17:43:52 +0100

    Add -g for mcf

--- mcf/compile-supmuc.sh
+++ mcf/compile-supmuc.sh
@@ -11,7 +11,7 @@ module load mpi.intel
 PREFIX=${HOME}/MCF/ppm/local_intelmpi_ifort121_o2_g
 
 ./configure --prefix=$HOME/MCF/mcf/mcf_install/ FC=ifort MPIFC=mpif90 \
-    LDFLAGS=-L${PREFIX}/lib/ FCFLAGS=-I${PREFIX}/include \
+    LDFLAGS=-L${PREFIX}/lib/ FCFLAGS="-I${PREFIX}/include -g" \
     MAKEDEPF90=${PREFIX}/bin/makedepf90 
 
 make -j 8
